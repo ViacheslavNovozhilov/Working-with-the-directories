8e414450049c7cdc555a2400928ac8d4814a09a5
ViacheslavNovozhilov <vyacheslavnovozhilov@gmail.com>
2024-08-03 11:10:11 +1000

    task

--- работа с папками и файлами.py
+++ работа с папками и файлами.py
@@ -1,71 +1,54 @@
-import os
-from pathlib import Path
-from pprint import pprint
-
-path_name = r'D:\\'
-point_path = os.path.join(path_name)
+import csv
+import re
 
 
-def list_content(path: str):
-    return sorted(os.listdir(path))
-
-
-lst = list_content(point_path)
-print(f"Отправная точка диск D - {list_content(point_path)}\n")
-
-
-def get_content_number(lst_content):
-    content_dict = {}
-    for i in range(len(lst_content)):
-        content_dict[i] = lst_content[i]
-    return content_dict
-
-
-pprint(get_content_number(lst))
-
-
-# проверка, является ли элемент файлом, если да, то читаем
-"""def is_file(dir_lst: list):
-    for elem in dir_lst:
-        if os.path.isfile(elem):
-            with open(elem, "r", encoding="utf-8") as file:
-                info = file.read()
-                return info
-
-
-def is_dir(dir_lst: list):
-    for elem in dir_lst:
-        if os.path.isdir(elem):
-            question()"""
-
-
-def choose_number(num):
-    lst_res = list_content(point_path)
-    dict_file_dir = get_content_number(lst_res)
-    for key, value in dict_file_dir.items():
-        if os.path.isfile(value):
-            new_start_point = os.path.join(point_path, value)
-            if num == key:
-                res = list_content(new_start_point)
-                return get_content_number(res)
-            with open(new_start_point, "r", encoding="utf-8") as file:
-                info = file.read()
-                return info
-
-
-def question(answer):
-    if answer == "y":
-        get_file_dir_number(int(input("Введите номер файла или каталога - ")))
-    elif answer == "n":
-        for num, elem in enumerate(list_content(point_path)):
-            print(num, elem)
+def registration():
+    email = input("Введите email: ")
+    login = input("Придумайте логин: ")
+    passwd = input("Придумайте пароль: ")
+    lst_data = [email, login, passwd]
+    email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    if re.match(email_pattern, email) is not None:
+        csv_create(lst_data)
     else:
-        print(f"Вы должны ввести y или n !")
+        print("Введите корректный email!")
 
 
-def get_file_dir_number(number):
-    res = choose_number(number)
-    pprint(res)
+def authorization(result):
+    log = input("Введите логин: ")
+    pas = input("Введите пароль: ")
+    for item in result:
+        if item.get("Логин") == log:
+            if item.get("Пароль") == pas:
+                print("Вы успешно авторизовались!")
+            else:
+                print("Введен не верный пароль!")
+        else:
+            print("Такого пользователя нет!")
 
 
-question(input("Хотите посмотреть содержимое каталога? - "))
+def csv_create(lst: list):
+    with open("data.csv", "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(lst)
+
+
+def csv_read() -> [dict]:
+    with open("data.csv", "r", encoding="utf-8") as file:
+        fieldnames = ["email", "Логин", "Пароль"]
+        reader = csv.DictReader(file, fieldnames=fieldnames)
+        row_list = []
+        for row in reader:
+            row_list.append(row)
+        return row_list
+
+
+answer = int(input("Введите 1, если хотите войти\nВведите 2, если хотите зарегистрироваться"))
+
+match answer:
+    case 1:
+        authorization(csv_read())
+    case 2:
+        registration()
+    case _:
+        print("Не верный ввод!")

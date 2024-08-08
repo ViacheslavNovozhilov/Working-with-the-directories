94f38978ad19e4baa17e4185b98a3658a51f5631
ViacheslavNovozhilov <vyacheslavnovozhilov@gmail.com>
2024-08-08 11:19:33 +1000

    Добавил проверку на наличие email и логин

--- работа с папками и файлами.py
+++ работа с папками и файлами.py
@@ -1,6 +1,5 @@
 import csv
 import re
-#клон для гита
 
 
 def registration():
@@ -11,6 +10,7 @@ def registration():
     email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
     if re.match(email_pattern, email) is not None:
         csv_create(lst_data)
+        check_login_exist(login, email)
     else:
         print("Введите корректный email!")
 
@@ -22,14 +22,16 @@ def authorization(result):
         if item.get("Логин") == log:
             if item.get("Пароль") == pas:
                 print("Вы успешно авторизовались!")
+                return
             else:
                 print("Введен не верный пароль!")
+                return
         else:
             print("Такого пользователя нет!")
 
 
 def csv_create(lst: list):
-    with open("data.csv", "w", encoding="utf-8") as file:
+    with open("data.csv", "a", newline='', encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerow(lst)
 
@@ -42,6 +44,15 @@ def csv_read() -> [dict]:
         for row in reader:
             row_list.append(row)
         return row_list
+
+
+def check_login_exist(login, email):
+    with open('data.csv', 'r', encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if login == row[1] or email == row[0]:
+                print("Пользователь с таким логином или email уже существует.")
+                return
 
 
 answer = int(input("Введите 1, если хотите войти\nВведите 2, если хотите зарегистрироваться"))
